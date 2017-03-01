3b60236c941d1e24b09046837187d528bd29303a
AdityaYuga <yugaapik@gmail.com>
2017-03-01 11:12:00 +0700

    first commit

---
+++ ay-youtube-fetcher.php
@@ -0,0 +1,16 @@
+<?php
+/**
+  * Plugin Name: AY Youtube Fetcher
+  * Plugin URI: https://github.com/adityayuga/ay-youtube-fetcher
+  * Description: This plugin fetch youtube videos from your youtube account
+  * Version: 1.0.0
+  * Author: Aditya Yuga Pradhana
+  * Author URI: https://github.com/adityayuga/
+  * License: MIT
+  */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
